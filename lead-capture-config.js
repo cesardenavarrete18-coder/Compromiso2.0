49da31151b456bec803112de328cc44f4f1be3cc
cesardenavarrete18-coder <cesardenavarrete18@gmail.com>
2026-06-10 14:59:47 -0300

    Configure Google Apps Script lead webhook

--- lead-capture-config.js
+++ lead-capture-config.js
@@ -1,4 +1,4 @@
 window.LEAD_CAPTURE_CONFIG = Object.freeze({
-  LEAD_WEBHOOK_URL: '',
+  LEAD_WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbw-2sQ_lhXRdPNN5ocXEEBIj3SrJLmGx3WjGI6S9TXkHlRscJc_d13fwZfQJceWHL7SVw/exec',
   WHATSAPP_FALLBACK_NUMBER: '5491141667220'
 });
